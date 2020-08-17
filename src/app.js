2ca770a222fa5c5a4f101fa278ecfdb786bab0ad
Daisy Annie <daisy.annie@shift.com.br>
2020-08-17 20:48:35 -0300

    Bootcamp - Desafio 02

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +10,76 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  response.send(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  repository = {
+    id: uuid(),
+    title,
+    url,
+    techs: techs,
+    likes: 0
+  }
+  repositories.push(repository);
+
+  response.send(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const { id } = request.params;
+
+  const repositoryId = repositories.findIndex(repository => repository.id === id);
+  if (repositoryId < 0){
+    return response.status(400).send({error: 'Id não encontrado'});
+  }
+
+  const oldRepository = repositories.find(repository => repository.id === id);
+  console.log(repositoryId);
+
+  repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes: oldRepository.likes
+  }
+  repositories[repositoryId] = repository;
+
+  response.send(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositoryId = repositories.findIndex(repository => repository.id === id);
+  if (repositoryId < 0){
+    return response.status(400).send({error: 'Id não encontrado'});
+  }
+  
+  repositories.splice(repositoryId,1)
+
+  response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositoryId = repositories.findIndex(repository => repository.id === id);
+  if (repositoryId < 0){
+    return response.status(400).send({error: 'Id não encontrado'});
+  }
+
+  const repository = repositories.find(repository => repository.id === id);
+
+  repository.likes++;  
+  repositories[repositoryId] = repository;
+
+  response.send(repository);
+
 });
 
 module.exports = app;
